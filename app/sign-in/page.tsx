0bfa3d0fe2b155c0e1908c3964e93427159791fa
hardtenderblade <matsenkodenis0@gmail.com>
2024-02-08 10:36:54 +0100

    login-screens

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,65 @@
+'use client'
+import { auth } from '@/firebase/main'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import React, { useEffect, useState } from 'react'
+
+export default function Page() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState<boolean | null>(null)
+
+    const [authenticated, setAuthenticated] = useState<boolean | null>(null)
+
+    useEffect(() => {
+        setAuthenticated(sessionStorage.getItem('authenticated') === 'true')
+    }, [])
+
+    const validateFields = () => {
+        if (email === '' || password === '') {
+            setError(true)
+            return false
+        }
+        return true
+    }
+
+    const handleSunmit = async () => {
+        if (!validateFields()) {
+            return
+        }
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                sessionStorage.setItem('authenticated', 'true')
+                setAuthenticated(true)
+            })
+            .catch((error) => {
+                setError(true)
+            })
+    }
+
+    return (
+        <div className="flex h-screen w-screen items-center justify-center">
+            <div className="flex flex-col bg-gray p-3">
+                {authenticated && <p className="text-green-500">You are authenticated</p>}
+
+                <h1 className="text-3xl font-bold">Here you can sign-in</h1>
+                <input
+                    type="text"
+                    placeholder="email"
+                    className="bg-white"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="password"
+                    className="bg-white"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                {error && <p className="text-red-500">Invalid credentials</p>}
+                <button onClick={handleSunmit}>Submit</button>
+            </div>
+        </div>
+    )
+}
